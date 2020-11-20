4cc808873604a679f68081d86058a8d151530987
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-20 17:40:09 +0000

    Update cv_with_flask.py

--- cv_with_flask.py
+++ cv_with_flask.py
@@ -1,24 +1,27 @@
 
-from flask import Flask, jsonify, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template
 import base.html
 import index.html
 # create an instance of our app
 app = Flask(__name__)
 
-@app.route("/")  # localhost:5000 is default port for Flask
+@app.route("/") 
 def home():
     # Homepage 
-    return "<h1>This is a dream team of DevOps consultants celebrating a WOW moment!!!</h1> "
+    return render_template(homepage.html)
 
 
-@app.route("/login/") #localhost:5000/welcome/
+# this doesn't work, need to fix
+@app.route("/login/")
 def greet_user():
-    return redirect(for_url('greet_user'))
+    return render_template(login.html)
 
 
 @app.route("/linkedin/")
 def linkedin():
     return render_template(linkedin.html)
 
-@app.route
-def 
+@app.route("/github/")
+def github():
+    return render_template(github.html)
+
